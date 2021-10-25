f1fe24d78ce50aaf4579c16b457972cebf4e6d75
maryam-mohebbi <88187675+maryam-mohebbi@users.noreply.github.com>
2021-10-25 20:55:37 +0330

    Add footer

--- src/App.js
+++ src/App.js
@@ -5,12 +5,43 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="papersheet ">
-        <div className="paperline">
-          <Dictionary />
+    <div className="Main">
+      <div className="App">
+        <div className="papersheet ">
+          <div className="paperline">
+            <Dictionary />
+          </div>
         </div>
       </div>
+      <footer className="Footer">
+        Coded by
+        <a
+          className="footer-link"
+          href="https://www.linkedin.com/in/maryam-mohebbi-61b905126/"
+          target="_blank"
+        >
+          {" "}
+          Maryam Mohebbi{" "}
+        </a>
+        and is open-sourced on
+        <a
+          className="footer-link"
+          href="https://github.com/Mary-SheCodes/dictionary-project"
+          target="_blank"
+        >
+          {" "}
+          GitHub{" "}
+        </a>
+        , hosted on
+        <a
+          className="footer-link"
+          href="https://app.netlify.com/"
+          target="_blank"
+        >
+          {" "}
+          Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 };
